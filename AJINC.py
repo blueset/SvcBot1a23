d527435347a1408596c9275c286c048ed4cd8ccb
Eana Hufwe <ilove@1a23.com>
2015-07-25 12:28:35 +0800

    Add timetable API in AJINCAPI

--- AJINC.py
+++ AJINC.py
@@ -16,6 +16,7 @@ class AJINCAPI(object):
 	__username = None
 	__password = None
 	__s = requests.Session()
+	__payload = {}
 
 	__response = None
 
@@ -23,6 +24,7 @@ class AJINCAPI(object):
 		self.__username = username
 		self.__password = password
 		r = self.__s.get("http://ajinc.wizlearn.com/ajinc")
+		self._save_viewstate(r.text)
 		sp = BeautifulSoup(r.text)
 		inputs = sp.find_all('input')
 
@@ -40,14 +42,21 @@ class AJINCAPI(object):
 				payload[item['name']] = item['value']
 		 
 		login = self.__s.post('http://ajinc.wizlearn.com/AjInc/login.aspx', data=payload)
+		self._save_viewstate(login.text)
 		loginsp = BeautifulSoup(login.text).find("font", color="Red")
 		if not loginsp == None:
 			raise AJINCAPILoginError(loginsp.text)
-		
+	
+	def _save_viewstate(self, text):
+		sp = BeautifulSoup(text)
+		inputs = sp.find_all('input')
+		for item in inputs:
+			if ("value" in item.attrs and item['name'].startswith('__')):
+				self.__payload[item['name']] = item['value']
 
 	def check_attendance(self, months=date.today().month, day=date.today().day):
 		attendance = self.__s.get('http://ajinc.wizlearn.com/ajinc/Student/Attendance/default.aspx')
-		 
+		self._save_viewstate(attendance.text)
 		at = BeautifulSoup(attendance.text)
 		atsp = at.find(id='ctl00_ContentArea_tblAttendance')
 		status = atsp.find_all('tr')[months].find_all('td')[day]['title']
@@ -76,3 +85,62 @@ class AJINCAPI(object):
 			#pprint(announcements[-1])
 
 		return announcements
+
+	def get_timetable(self, tdate=date.today()):
+		# TODO: check on date.
+		
+		html = self.__s.get("http://ajinc.wizlearn.com/ajinc/Student/TimeTable/default.aspx")
+		self._save_viewstate(html.text)
+		import calendar
+		last_day = calendar.monthrange(tdate.year, tdate.month)[1]
+		payload = {
+			'__EVENTTARGET': 'ctl00$ContentArea$btnGo',
+			'__EVENTARGUMENT': 'btnGo',
+			'ctl00_ContentArea_dpDate_picker_selecteddates': tdate.strftime("%Y.%-m.%-d.0.0.0"),
+			'ctl00_ContentArea_dpDate_picker_visibledate': date.today().strftime("%Y.%-m.%-d"),
+			'ctl00_ContentArea_dpDate_picker_picker': tdate.strftime("%a, %-d-%-b-%Y"),
+			'ctl00_SchoolBuzzTopMenu_ContextData': '',
+			'ctl00_ContentArea_dpDate_calendar_apparentvisibledate': date.today().strftime("%Y.%-m"),
+			'ctl00_ContentArea_dpDate_calendar_selecteddates': tdate.strftime("%Y.%-m."+str(last_day)),
+			'ctl00_ContentArea_dpDate_calendar_visibledate': date.today().strftime("%Y.%-m.%-d"),
+		}
+		payload.update(self.__payload)
+		html = self.__s.post("http://ajinc.wizlearn.com/ajinc/Student/TimeTable/default.aspx", data=payload)
+		self._save_viewstate(html.text)
+		#html = self.__s.get("http://ajinc.wizlearn.com/ajinc/Student/TimeTable/default.aspx")
+
+		soup = BeautifulSoup(html.text)
+		trs = soup.find(id="ctl00_ContentArea_tblTimeSlots").find_all("tr")
+		tbl = []
+		day = []
+		subject = {"name": [], "venue": [], "type": "", "span": 0}
+		empty = {"name": [], "venue": [], "type": "empty", "span": 0}
+		for tr in trs[1:]:
+			trday = day.copy()
+			for td in tr.find_all("td")[1:]:
+				span = int(td.attrs['colspan'])
+				if len(td.contents) == 0: # empty day
+					if len(trday) == 0:
+						trday.append(empty.copy())
+						trday[-1]['span'] = span
+						pass
+					if trday[-1]['type'] == 'empty':
+						trday[-1]['span'] += span
+						pass
+					else:
+						trday.append(empty.copy())
+						trday[-1]['span'] = span
+						pass
+				else:
+					name = []
+					venue = []
+					for i in range(int(len(td.contents)/4)):
+						name.append(str(td.contents[i*4]))
+						venue.append(str(td.contents[i*4+2]))
+					trday.append(subject.copy())
+					trday[-1]['name']=name
+					trday[-1]['venue']=venue
+					trday[-1]['span']=int(td.attrs['colspan'])
+					trday[-1]['type']=td.attrs['title']
+			tbl.append(trday.copy())
+		return tbl	
